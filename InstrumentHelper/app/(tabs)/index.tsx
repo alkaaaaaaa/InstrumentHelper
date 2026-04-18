631f7035c76fb418c7b20f8cb3524e1e108486aa
alkaaaaaaa <alexanderqiuqiu@gmail.com>
2026-04-18 21:32:50 +0800

    背景图

--- InstrumentHelper/app/(tabs)/index.tsx
+++ InstrumentHelper/app/(tabs)/index.tsx
@@ -1,23 +1,34 @@
 import { View, Button } from 'tamagui'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, ImageBackground } from 'react-native'
 import { useRouter } from 'expo-router'
 
 export default function Home() {
     const router = useRouter()
     return (
-        <View style={styles.container}>
-            <Button onPress={() => router.push('/score')}>乐谱查看&编写</Button>
-            <Button onPress={() => router.push('/learn')}>学习</Button>
-        </View>
+        <ImageBackground
+            source={require('../../assets/background.jpg')}
+            style={styles.background}
+            resizeMode="cover"
+        >
+            <View style={styles.container}>
+                <Button onPress={() => router.push('/score')}>乐谱查看&编写</Button>
+                <Button onPress={() => router.push('/learn')}>学习</Button>
+            </View>
+        </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
     container: {
-        flexGrow: 1,
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'white',
+        backgroundColor: 'transparent',
         gap: 10
     }
 })
